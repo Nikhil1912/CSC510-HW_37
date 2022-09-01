e3917b1846429f8c7993fef83e4a9c79b8a4fcc9
insamuel <i_samuelsson@yahoo.com>
2022-09-01 15:42:09 -0400

    GIT-6 Added default int value for has dict keys

--- code/Sym.py
+++ code/Sym.py
@@ -1,14 +1,16 @@
 # Sym's summarize a stream of symbols.
+from collections import defaultdict
+
+
 class Sym:
     def __init__(self, col_position=0, col_name=""):
         self.num_items = 0                                  # items seen
         self.col_position = col_position                    # column position
         self.col_name = col_name                            # column name
-        self.has = {}                                       # kept data
+        self.has = defaultdict(0)                           # kept data
 
     # Add symbol value to Sym object
     def add(self, value):
         if value != "?":
             self.num_items += 1
-            self.has.setdefault(value, 0)
             self.has[value] += 1

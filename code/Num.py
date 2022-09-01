a518f7ba7b7e8794fe150d882efac9806fbf7baa
Nikhil Mehra <nmehra2@ncsu.edu>
2022-09-01 15:24:34 -0400

    Created the Num class and its constructor

---
+++ code/Num.py
@@ -0,0 +1,14 @@
+import math
+
+
+# Num summarizes a stream of numbers
+class Num:
+    def __init__(self, col_position=0, col_name=""):
+        self.num_items = 0                                  # items seen
+        self.col_position = col_position                    # column position
+        self.col_name = col_name                            # column name
+        self.has = {}                                       # kept data
+        self.lo = math.inf                                  # lowest seen
+        self.hi = -math.inf                                 # highest seen
+        self.is_sorted = True                               # no updates since last sort of data
+        self.w = 1 if col_name and col_name[-1] == '+' else -1

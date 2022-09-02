5c0d7ec3d240462451c8edf58167d70a8a200407
M M Abid Naziri <abidnazirisami@gmail.com>
2022-09-02 19:24:57 -0400

    GIT-9 Added nums function to Num class

--- code/Num.py
+++ code/Num.py
@@ -12,3 +12,11 @@ class Num:
         self.hi = -math.inf                                 # highest seen
         self.is_sorted = True                               # no updates since last sort of data
         self.w = 1 if col_name and col_name[-1] == '+' else -1      # maximize or minimize column
+
+    # Get the stored numbers in a sorted order
+    def nums(self):
+        if not self.is_sorted:
+            self.has.sort()
+            self.is_sorted = True
+        
+        return self.has

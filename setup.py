15e03b1f94107056a2919c0b02263d017a68c036
M M Abid Naziri <abidnazirisami@gmail.com>
2022-10-06 18:01:24 -0400

    GIT-88 Added the setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='CSC510-HW_37',
+    version='1.0.0',
+    url='https://github.com/Nikhil1912/CSC510-HW_37',
+    description='CSC 510 - Fall 22 HW - Group 37',
+)
